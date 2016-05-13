fd8ded4bc5f54fd3e3c6b80b8ef139e776f3ee39
Kat Marchán <kzm@sykosomatic.org>
2016-05-12 19:29:07 -0700

    ls: fix search for prerelease versions
    Fixes: https://github.com/npm/npm/issues/9436

--- test/tap/ls.js
+++ test/tap/ls.js
@@ -152,6 +152,51 @@ test('ls with missing filtered arg', function (t) {
   })
 })
 
+test('ls with prerelease pkg', function (t) {
+  var fixture = new Tacks(
+    Dir({
+      'npm-test-ls': Dir({
+        'package.json': File({
+          name: 'npm-test-ls',
+          version: '1.0.0',
+          dependencies: {
+            'dep': 'file:../dep'
+          }
+        })
+      }),
+      'dep': Dir({
+        'package.json': File({
+          name: 'dep',
+          version: '1.0.0-pre'
+        })
+      })
+    })
+  )
+  fixture.create(fixturepath)
+  common.npm([
+    'install'
+  ], {
+    cwd: path.join(fixturepath, 'npm-test-ls')
+  }, function (err, code) {
+    t.ifErr(err, 'install succeeded')
+    t.equal(0, code, 'exit 0 on install')
+    common.npm([
+      'ls', 'dep',
+      '--json'
+    ], {
+      cwd: path.join(fixturepath, 'npm-test-ls')
+    }, function (err, code, stdout, stderr) {
+      t.ifErr(err, 'ls succeeded')
+      t.equal(0, code, 'exit 0 on ls')
+      var pkg = JSON.parse(stdout)
+      var deps = pkg.dependencies
+      t.ok(deps.dep, 'dep present')
+      fixture.remove(fixturepath)
+      t.done()
+    })
+  })
+})
+
 test('cleanup', function (t) {
   rimraf.sync(basepath)
   t.done()
